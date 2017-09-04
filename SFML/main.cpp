0983a90af063622eab76ebb89aba911aa45da55d
kylek <kylek@DESKTOP-QRGK6VL>
2017-09-04 19:41:31 -0400

    Add project files.

---
+++ SFML/main.cpp
@@ -0,0 +1,23 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			std::cout << event.type << std::endl;
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
